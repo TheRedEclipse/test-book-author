d65f21ab2c9adb2ed360e71e1b57c9a1d8799c28
RedEclipse <redeclipse@yandex.ru>
2022-11-10 20:44:24 +0300

    Typo in index blade fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Authors - Books relatinoship test work</title>
+        <title>Authors - Books relationship test work</title>
         @vite(['resources/js/app.js', 'resources/js/app.css'])
     </head>
     <body>
